4ac0312b9bfd659c2ca1d8fa996261b43a7ef65e
Karthik R S <karthikrs@microsoft.com>
2022-06-27 16:04:05 +0530

    website push

---
+++ site/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { Meta, Title } from "@angular/platform-browser";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+
+  title: string = "Live Resume - Karthik R S";
+
+  constructor(
+    private titleService: Title,
+    private metaTagService: Meta
+  ) { }
+
+  ngOnInit(): void {
+
+    this.titleService.setTitle(this.title);
+
+    this.metaTagService.addTags([
+      { name: "keywords", content: "Web Developer, Java Developer, Full-stack Java Developer, DevOps Engineer, Android Developer, Karthik R S Live Resume, Karthik R S Resume, Karthik R S CV, Curriculum Karthik R S, GBASTOS Resumo, gbastos live resume" },
+      { name: "robots", content: "index, follow" },
+      { name: "author", content: "Karthik R S" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "date", content: "2020-05-12", scheme: "YYYY-MM-DD" },
+      { charset: "UTF-8" }
+    ]);
+
+    this.metaTagService.updateTag(
+      { name: "description", content: "Hello, I'm a Full-stack Web Developer with 10+ years of experience designing web and mobile projects. Find out more in my live-resume!" }
+    );
+  }
+}
---
+++ site/src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  production: true,
+  // replace the data below with your personal data
+  personal: {
+    caricature: "1.png",
+    name: "Karthik R S",
+    birth: "1993-12-20",
+    email: "rsk1993@hotmail.com",
+    phone: "+91 9666665914",
+    location: "Hyderabad, India"
+  },
+  // replace the dummy data below with the real firebase configs  
+  firebaseConfig: {
+    apiKey: "AIzaSyAUhN1dQNMr4q5lvVQCl1jeBrKWa69rrXM",
+    authDomain: "personalwebsite-456f2.firebaseapp.com",
+    databaseURL: "https://personalwebsite-456f2.firebaseio.com",
+    projectId: "personalwebsite-456f2",
+    storageBucket: "personalwebsite-456f2.appspot.com",
+    messagingSenderId: "281132887974",
+    appId: "1:281132887974:web:d749c0ad957aa83ced6967",
+    measurementId: "G-RD5VEDPYJL"
+  }
+};
